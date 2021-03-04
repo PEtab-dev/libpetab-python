c9df7ed92cf677066d569e8f3167923c2028a7de
Paul Jonas Jost <jost.pauljonas@gmail.com>
2021-03-04 09:36:22 +0100

    not finished FirstPlot

--- petab/visualize/plotting.py
+++ petab/visualize/plotting.py
@@ -49,17 +49,17 @@ class DataToPlot:
 
 class SinglePlot:
     def __init__(self,
-                 vis_spec,
+                 plot_spec,
                  measurements_df: Optional[pd.DataFrame],
                  simulations_df: Optional[pd.DataFrame]):
         self.id = None
-        self.vis_spec = None  # dataframe, vis spec of a single plot
+        self.plot_spec = None  # dataframe, vis spec of a single plot
 
         # if both meas and simu dfs are None error
         self.measurements_to_plot = self.get_measurements_to_plot()  # dataframe?
         self.simulations_to_plot = self.get_simulations_to_plot()
 
-        self.xValues = vis_spec[X_VALUES]
+        self.xValues = plot_spec[X_VALUES]
 
         # parameters of a specific plot
         self.title = ''
@@ -67,7 +67,59 @@ class SinglePlot:
         self.yLabel = Y_LABEL
 
     def get_measurements_to_plot(self) -> Optional[pd.DataFrame]:
-        return None
+
+        # create empty dataframe for means and SDs
+        meas_to_plot = pd.DataFrame(
+            columns=['mean', 'noise_model', 'sd', 'sem', 'repl'],
+            index=condition_ids
+        )
+        for var_cond_id in condition_ids:
+
+            # TODO (#117): Here not the case: So, if entries in measurement file:
+            #  preequCondId, time, observableParams, noiseParams,
+            #  are not the same, then  -> differ these data into
+            #  different groups!
+            # now: go in simulationConditionId, search group of unique
+            # simulationConditionId e.g. rows 0,6,12,18 share the same
+            # simulationCondId, then check if other column entries are the same
+            # (now: they are), then take intersection of rows 0,6,12,18 and checked
+            # other same columns (-> now: 0,6,12,18) and then go on with code.
+            # if there is at some point a difference in other columns, say e.g.
+            # row 12,18 have different noiseParams than rows 0,6, the actual code
+            # would take rows 0,6 and forget about rows 12,18
+
+            # compute mean and standard deviation across replicates
+            subset = matches_plot_spec(m_data, col_id, var_cond_id, plot_spec)
+            data_measurements = m_data.loc[
+                subset,
+                MEASUREMENT
+            ]
+
+            meas_to_plot.at[var_cond_id, 'mean'] = np.mean(data_measurements)
+            meas_to_plot.at[var_cond_id, 'sd'] = np.std(data_measurements)
+
+            if (plot_spec.plotTypeData == PROVIDED) & sum(subset):
+                if len(m_data.loc[subset, NOISE_PARAMETERS].unique()) > 1:
+                    raise NotImplementedError(
+                        f"Datapoints with inconsistent {NOISE_PARAMETERS} is "
+                        f"currently not implemented. Stopping.")
+                tmp_noise = m_data.loc[subset, NOISE_PARAMETERS].values[0]
+                if isinstance(tmp_noise, str):
+                    raise NotImplementedError(
+                        "No numerical noise values provided in the measurement "
+                        "table. Stopping.")
+                if isinstance(tmp_noise, Number) or tmp_noise.dtype == 'float64':
+                    meas_to_plot.at[var_cond_id, 'noise_model'] = tmp_noise
+
+            # standard error of mean
+            meas_to_plot.at[var_cond_id, 'sem'] = \
+                np.std(data_measurements) / np.sqrt(len(data_measurements))
+
+            # single replicates
+            meas_to_plot.at[var_cond_id, 'repl'] = \
+                data_measurements
+
+        return meas_to_plot
 
     def get_simulations_to_plot(self) -> Optional[pd.DataFrame]:
         return None
