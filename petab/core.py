11da361e09ae1b0afb17104be973be3a3c325c66
FFroehlich <fabian@schaluck.com>
2021-03-19 11:48:01 -0400

    fix replacement id construction

--- petab/core.py
+++ petab/core.py
@@ -139,8 +139,8 @@ def flatten_timepoint_specific_output_overrides(
         replacement_id = ''
         for field in possible_groupvars:
             if field in groupvars:
-                val = groupvar[groupvars.index(field)
-                               ].replace(';', '_').replace('.', '_')
+                val = str(groupvar[groupvars.index(field)
+                                   ]).replace(';', '_').replace('.', '_')
                 if replacement_id == '':
                     replacement_id = val
                 elif val != '':
