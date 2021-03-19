8fd90880ea16d3c3e7e47cbfc640b89e1c113d26
FFroehlich <fabian@schaluck.com>
2021-03-19 17:12:45 -0400

    fixups

--- petab/lint.py
+++ petab/lint.py
@@ -622,7 +622,7 @@ def observable_table_has_nontrivial_noise_formula(
     """
 
     return not observable_df[NOISE_FORMULA].apply(
-        lambda x: re.match(r'^[\w_\.]+$', x) is not None
+        lambda x: re.match(r'^[\w_\.]+$', str(x)) is not None
     ).all()
 
 
