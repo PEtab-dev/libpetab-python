c717a2d7045de377fa24dade97c16a7a041460b7
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2021-03-22 15:58:05 +0100

    add_stylesheet

--- doc/conf.py
+++ doc/conf.py
@@ -78,8 +78,7 @@ source_suffix = {
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-def setup(app):
-    app.add_stylesheet('custom.css')
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
