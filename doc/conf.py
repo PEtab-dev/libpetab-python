cb2cecce55daf4561520c7fa50ac5953629e7053
Polina Lakrisenko <p.lakrisenko@gmail.com>
2021-05-25 16:18:09 +0200

    sphinx env variable

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ release = 'latest'
 
 # -- Custom pre-build --------------------------------------------------------
 
-
+os.environ["SPHINX_APIDOC_OPTIONS"] = "members,show-inheritance"
 subprocess.run(['python', 'md2rst.py'])
 
 # -- General configuration ---------------------------------------------------
