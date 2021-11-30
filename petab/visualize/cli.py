7c829628178a39c3f0d8285e1cc3894d945d0c02
Daniel Weindl <dweindl@users.noreply.github.com>
2021-11-30 17:55:18 +0100

    Apply suggestions from code review
    Co-authored-by: Dilan Pathirana <59329744+dilpath@users.noreply.github.com>

--- petab/visualize/cli.py
+++ petab/visualize/cli.py
@@ -1,4 +1,4 @@
-"""Command line interface for visualization"""
+"""Command-line interface for visualization."""
 import argparse
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -7,13 +7,13 @@ from .. import Problem, get_simulation_df, get_visualization_df
 
 
 def _parse_cli_args():
-    """Parse command line arguments"""
+    """Parse command-line arguments."""
 
     parser = argparse.ArgumentParser(
         description='Create PEtab visualizations.')
 
     parser.add_argument('-y', '--yaml', dest='yaml_file_name', required=True,
-                        help='PEtab YAML problem filename')
+                        help='PEtab problem YAML filename')
     parser.add_argument('-s', '--simulations', dest='simulation_file_name',
                         required=False,
                         help='PEtab simulation filename')
@@ -29,14 +29,13 @@ def _parse_cli_args():
 
 
 def _petab_visualize_main():
-    """Entrypoint for visualization command line interface"""
+    """Entrypoint for visualization command-line interface."""
     args = _parse_cli_args()
 
     petab_problem = Problem.from_yaml(args.yaml_file_name)
+    simulations_df = None
     if args.simulation_file_name:
         simulations_df = get_simulation_df(args.simulation_file_name)
-    else:
-        simulations_df = None
 
     if args.visualization_file_name:
         petab_problem.visualization_df = get_visualization_df(
@@ -45,7 +44,7 @@ def _petab_visualize_main():
     if args.style_file_name:
         plt.style.use(args.style_file_name)
 
-    Path(args.output_directory).mkdir(exist_ok=True)
+    Path(args.output_directory).mkdir(exist_ok=True, parents=True)
 
     plot_problem(
         petab_problem=petab_problem,
