4061ec7e2bca736305eb2457d3bc02418b13dde2
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2022-02-02 18:01:54 +0100

    Fix pandas deprecation warning
    FutureWarning: The frame.append method is deprecated and will be removed from pandas in a future version. Use pandas.concat instead.

--- petab/core.py
+++ petab/core.py
@@ -209,7 +209,7 @@ def concat_tables(
         if isinstance(tmp_df, (str, Path)):
             tmp_df = file_parser(tmp_df)
 
-        df = df.append(tmp_df, sort=False,
+        df = pd.concat([df, tmp_df], sort=False,
                        ignore_index=isinstance(tmp_df.index, pd.RangeIndex))
 
     return df
