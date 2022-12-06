e453ea320ac160440216bdb7610f64df42652e00
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2022-12-06 14:04:06 +0100

    petablint

--- petab/lint.py
+++ petab/lint.py
@@ -842,6 +842,10 @@ def lint_problem(problem: 'petab.Problem') -> bool:
             logger.error(e)
             errors_occurred = True
 
+    if problem.visualization_df is not None:
+        from petab.visualize.lint import validate_visualization_df
+        errors_occurred |= validate_visualization_df(problem)
+
     if errors_occurred:
         logger.error('Not OK')
     elif problem.measurement_df is None or problem.condition_df is None \
