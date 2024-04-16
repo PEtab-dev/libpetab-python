117b77f057846687ead065a26466eb2af6c7575c
Daniel Weindl <daniel.weindl@helmholtz-munich.de>
2024-04-16 08:59:06 +0200

    ..

--- petab/visualize/__init__.py
+++ petab/visualize/__init__.py
@@ -6,15 +6,25 @@ PEtab comes with visualization functionality. Those need to be imported via
 ``import petab.visualize``.
 
 """
+# ruff: noqa: F401
 import importlib.util
-
-mpl_spec = importlib.util.find_spec("matplotlib")
 
 from .plotting import DataProvider, Figure
 
 __all__ = ["DataProvider", "Figure"]
 
-if mpl_spec is not None:
+if importlib.util.find_spec("matplotlib") is not None:
+    from .plot_data_and_simulation import (
+        plot_problem,
+        plot_with_vis_spec,
+        plot_without_vis_spec,
+    )
+    from .plot_residuals import (
+        plot_goodness_of_fit,
+        plot_residuals_vs_simulation,
+    )
+    from .plotter import MPLPlotter
+
     __all__.extend(
         [
             "plot_without_vis_spec",
