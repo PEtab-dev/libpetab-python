018d8d1d118ca8835e1d424812a6be4725853987
Daniel Weindl <daniel.weindl@helmholtz-munich.de>
2024-06-27 10:59:47 +0200

    v1/v2 subpackage

---
+++ petab/v1/__init__.py
@@ -0,0 +1,5 @@
+"""The PEtab 1.0 subpackage.
+
+Contains all functionality related to handling PEtab 1.0 problems.
+"""
+from .. import *
---
+++ petab/v2/__init__.py
@@ -0,0 +1,4 @@
+"""The PEtab 2.0 subpackage.
+
+Contains all functionality related to handling PEtab 2.0 problems.
+"""
