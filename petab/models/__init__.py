2631cc7885856ccd3cdc6ac64cea614bf7a070a9
Daniel Weindl <daniel.weindl@helmholtz-munich.de>
2024-06-29 17:32:36 +0200

    ..

--- petab/models/__init__.py
+++ petab/models/__init__.py
@@ -1,3 +1,6 @@
+"""Deprecated module for PEtab models.
+
+Use petab.v1.models instead"""
 from petab import _deprecated_import_v1
 from petab.v1.models import *  # noqa: F403, F401, E402
 
--- petab/models/pysb_model.py
+++ petab/models/pysb_model.py
@@ -1,3 +1,6 @@
+"""Deprecated module for PySB models.
+
+Use petab.v1.models.pysb_model instead."""
 from petab import _deprecated_import_v1
 from petab.v1.models.pysb_model import *  # noqa: F403, F401, E402
 
