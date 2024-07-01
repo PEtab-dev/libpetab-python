61e9720df469446b9dc99a194717e25ea5f58acd
Daniel Weindl <daniel.weindl@helmholtz-munich.de>
2024-07-01 18:42:59 +0200

    ..

--- tests/test_deprecation_warnings.py
+++ tests/test_deprecation_warnings.py
@@ -12,8 +12,13 @@ def test_deprecated_global():
 
         petab.Problem()
 
+    with pytest.warns(DeprecationWarning):
+        import petab.parameters
+
     with warnings.catch_warnings():
         warnings.simplefilter("error")
         from petab.v1 import Problem  # noqa
 
         Problem()
+
+        import petab.v1.parameters
