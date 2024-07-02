PEtab-dev/libpetab-python

commit c9074f73e2b8332787f99040bebf4039ab09447a

Implement `petab.*` deprecation with `__getattr__` (#297)
Co-authored-by: Daniel Weindl <dweindl@users.noreply.github.com>
Co-authored-by: Daniel Weindl <daniel.weindl@helmholtz-munich.de>

petab/__init__.py
@@ -2,74 +2,66 @@
 PEtab global
 ============
 
-.. warning::
-
-    All functions in here are deprecated. Use the respective functions from
-    :mod:`petab.v1` instead.
-
 Attributes:
     ENV_NUM_THREADS:
         Name of environment variable to set number of threads or processes
         PEtab should use for operations that can be performed in parallel.
         By default, all operations are performed sequentially.
 """
-import functools
-import inspect
+import importlib
 import sys
-import warnings
+from functools import partial
+from pathlib import Path
 from warnings import warn
 
-# deprecated imports
-from petab.v1 import *  # noqa: F403, F401, E402
-
-from .v1.format_version import __format_version__  # noqa: F401, E402
-
-# __all__ = [
-#     'ENV_NUM_THREADS',
-# ]
-
 ENV_NUM_THREADS = "PETAB_NUM_THREADS"
+__all__ = ["ENV_NUM_THREADS"]
 
 
-def _deprecated_v1(func):
-    """Decorator for deprecation warnings for functions."""
-
-    @functools.wraps(func)
-    def new_func(*args, **kwargs):
-        warnings.warn(
-            f"petab.{func.__name__} is deprecated, "
-            f"please use petab.v1.{func.__name__} instead.",
-            category=DeprecationWarning,
-            stacklevel=2,
+def __getattr__(name):
+    if attr := globals().get(name):
+        return attr
+    if name == "v1":
+        return importlib.import_module("petab.v1")
+    if name != "__path__":
+        warn(
+            f"Accessing `petab.{name}` is deprecated and will be removed in "
+            f"the next major release. Please use `petab.v1.{name}` instead.",
+            DeprecationWarning,
+            stacklevel=3,
         )
-        return func(*args, **kwargs)
-
-    return new_func
+    return getattr(importlib.import_module("petab.v1"), name)
 
 
-def _deprecated_import_v1(module_name: str):
-    """Decorator for deprecation warnings for modules."""
-    warn(
-        f"The '{module_name}' module is deprecated and will be removed "
-        f"in the next major release. Please use "
-        f"'petab.v1.{module_name.removeprefix('petab.')}' "
-        "instead.",
-        DeprecationWarning,
-        stacklevel=2,
+def v1getattr(name, module):
+    if name != "__path__":
+        warn(
+            f"Accessing `petab.{name}` is deprecated and will be removed in "
+            f"the next major release. Please use `petab.v1.{name}` instead.",
+            DeprecationWarning,
+            stacklevel=3,
+        )
+    try:
+        return module.__dict__[name]
+    except KeyError:
+        raise AttributeError(name) from None
+
+
+# Create dummy modules for all old modules
+v1_root = Path(__file__).resolve().parent / "v1"
+v1_objects = [f.relative_to(v1_root) for f in v1_root.rglob("*")]
+for v1_object in v1_objects:
+    if "__pycache__" in str(v1_object):
+        continue
+    if v1_object.suffix not in ["", ".py"]:
+        continue
+    if not (v1_root / v1_object).exists():
+        raise ValueError(v1_root / v1_object)
+    v1_object_parts = [*v1_object.parts[:-1], v1_object.stem]
+    module_name = ".".join(["petab", *v1_object_parts])
+
+    real_module = importlib.import_module(
+        f"petab.v1.{'.'.join(v1_object_parts)}"
     )
-
-
-__all__ = [
-    x
-    for x in dir(sys.modules[__name__])
-    if not x.startswith("_")
-    and x not in {"sys", "warnings", "functools", "warn", "inspect"}
-]
-
-
-# apply decorator to all functions in the module
-for name in __all__:
-    obj = globals().get(name)
-    if callable(obj) and inspect.isfunction(obj):
-        globals()[name] = _deprecated_v1(obj)
-del name, obj
+    real_module.__getattr__ = partial(v1getattr, module=real_module)
+    sys.modules[module_name] = real_module

petab/math/__init__.py
@@ -1,9 +1,0 @@
-"""Deprecated module for math handling.
-
-Use petab.v1.math instead."""
-from petab import _deprecated_import_v1
-from petab.v1.math import *  # noqa: F403, F401, E402
-
-from .sympify import sympify_petab  # noqa: F401
-
-_deprecated_import_v1(__name__)

petab/models/__init__.py
@@ -1,7 +1,0 @@
-"""Deprecated module for PEtab models.
-
-Use petab.v1.models instead"""
-from petab import _deprecated_import_v1
-from petab.v1.models import *  # noqa: F403, F401, E402
-
-_deprecated_import_v1(__name__)

petab/visualize/__init__.py
@@ -1,10 +1,0 @@
-"""Deprecated module for visualization of PEtab problems.
-
-Use petab.v1.visualize instead."""
-
-from petab import _deprecated_import_v1
-from petab.v1.visualize import *  # noqa: F403, F401, E402
-
-from .plotting import DataProvider, Figure  # noqa: F403, F401, E402
-
-_deprecated_import_v1(__name__)

petab/C.py
@@ -1,5 +1,0 @@
-"""Deprecated module. Use petab.v1.C instead."""
-from petab import _deprecated_import_v1
-from petab.v1.C import *  # noqa: F403, F401, E402
-
-_deprecated_import_v1(__name__)

petab/calculate.py
@@ -1,7 +1,0 @@
-"""Deprecated module for calculating residuals and log-likelihoods.
-
-Use petab.v1.calculate instead."""
-from petab import _deprecated_import_v1
-from petab.v1.calculate import *  # noqa: F403, F401, E402
-
-_deprecated_import_v1(__name__)

petab/composite_problem.py
@@ -1,7 +1,0 @@
-"""Deprecated module for composite problems.
-
-Use petab.v1.composite_problem instead."""
-from petab import _deprecated_import_v1
-from petab.v1.composite_problem import *  # noqa: F403, F401, E402
-
-_deprecated_import_v1(__name__)

petab/conditions.py
@@ -1,8 +1,0 @@
-"""Deprecated module for condition tables.
-
-Use petab.v1.conditions instead.
-"""
-from petab import _deprecated_import_v1
-from petab.v1.conditions import *  # noqa: F403, F401, E402
-
-_deprecated_import_v1(__name__)

commit 9447f788fad4a0bb59b9866022fa87e29ed89bee

Fix imports
Fixes an issue introduced in #297, which made all optional dependencies required.

petab/__init__.py
@@ -60,8 +60,11 @@ for v1_object in v1_objects:
     v1_object_parts = [*v1_object.parts[:-1], v1_object.stem]
     module_name = ".".join(["petab", *v1_object_parts])
 
-    real_module = importlib.import_module(
-        f"petab.v1.{'.'.join(v1_object_parts)}"
-    )
-    real_module.__getattr__ = partial(v1getattr, module=real_module)
-    sys.modules[module_name] = real_module
+    try:
+        real_module = importlib.import_module(
+            f"petab.v1.{'.'.join(v1_object_parts)}"
+        )
+        real_module.__getattr__ = partial(v1getattr, module=real_module)
+        sys.modules[module_name] = real_module
+    except ModuleNotFoundError:
+        pass