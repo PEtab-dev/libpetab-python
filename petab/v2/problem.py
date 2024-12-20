6a9ecd0088a59e842206d800f5013fe8d9e00f1c
Daniel Weindl <dweindl@users.noreply.github.com>
2024-12-20 12:13:35 +0100

    Fix version checks (#350)
    Add function for parsing version numbers. Update checks.
    
    Fixes #349.

--- petab/v2/problem.py
+++ petab/v2/problem.py
@@ -28,6 +28,7 @@ from ..v1.models.model import Model, model_factory
 from ..v1.problem import ListOfFiles, VersionNumber
 from ..v1.yaml import get_path_prefix
 from ..v2.C import *  # noqa: F403
+from ..versions import parse_version
 from . import conditions, experiments
 
 if TYPE_CHECKING:
@@ -161,14 +162,15 @@ class Problem:
                 return filename
             return f"{base_path}/{filename}"
 
-        if yaml_config[FORMAT_VERSION] not in {"2.0.0"}:
+        if (format_version := parse_version(yaml_config[FORMAT_VERSION]))[
+            0
+        ] != 2:
             # If we got a path to a v1 yaml file, try to auto-upgrade
             from tempfile import TemporaryDirectory
 
-            from ..versions import get_major_version
             from .petab1to2 import petab1to2
 
-            if get_major_version(yaml_config) == 1 and yaml_file:
+            if format_version[0] == 1 and yaml_file:
                 logging.debug(
                     "Auto-upgrading problem from PEtab 1.0 to PEtab 2.0"
                 )
@@ -185,7 +187,7 @@ class Problem:
                     )
             raise ValueError(
                 "Provided PEtab files are of unsupported version "
-                f"{yaml_config[FORMAT_VERSION]}. Expected 2.0.0."
+                f"{yaml_config[FORMAT_VERSION]}."
             )
 
         if yaml.is_composite_problem(yaml_config):
--- petab/versions.py
+++ petab/versions.py
@@ -1,35 +1,62 @@
 """Handling of PEtab version numbers."""
 from __future__ import annotations
 
+import re
 from pathlib import Path
 
 import petab
-from petab.v1 import Problem as V1Problem
-from petab.v1.C import FORMAT_VERSION
-from petab.v1.yaml import load_yaml
 
 __all__ = [
     "get_major_version",
+    "parse_version",
 ]
+from . import v1
+
+# version regex pattern
+_version_pattern = (
+    r"(?P<major>\d+)(?:\.(?P<minor>\d+))?"
+    r"(?:\.(?P<patch>\d+))?(?P<suffix>[\w.]+)?"
+)
+_version_re = re.compile(_version_pattern)
+
+
+def parse_version(version: str | int) -> tuple[int, int, int, str]:
+    """Parse a version string into a tuple of integers and suffix."""
+    if isinstance(version, int):
+        return version, 0, 0, ""
+
+    version = str(version)
+    match = _version_re.match(version)
+    if match is None:
+        raise ValueError(f"Invalid version string: {version}")
+
+    major = int(match.group("major"))
+    minor = int(match.group("minor") or 0)
+    patch = int(match.group("patch") or 0)
+    suffix = match.group("suffix") or ""
+
+    return major, minor, patch, suffix
 
 
 def get_major_version(
-    problem: str | dict | Path | V1Problem | petab.v2.Problem,
+    problem: str | dict | Path | petab.v1.Problem | petab.v2.Problem,
 ) -> int:
     """Get the major version number of the given problem."""
     version = None
 
     if isinstance(problem, str | Path):
+        from petab.v1.yaml import load_yaml
+
         yaml_config = load_yaml(problem)
-        version = yaml_config.get(FORMAT_VERSION)
+        version = yaml_config.get(v1.C.FORMAT_VERSION)
     elif isinstance(problem, dict):
-        version = problem.get(FORMAT_VERSION)
+        version = problem.get(v1.C.FORMAT_VERSION)
 
     if version is not None:
         version = str(version)
         return int(version.split(".")[0])
 
-    if isinstance(problem, V1Problem):
+    if isinstance(problem, petab.v1.Problem):
         return 1
 
     from . import v2
