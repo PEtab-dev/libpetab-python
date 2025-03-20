51c96adca35c8cf47369644bbac6ff44434e3bf2
Daniel Weindl <daniel.weindl@uni-bonn.de>
2025-03-20 14:38:00 +0100

    Add petab-compatible sympy string-printer
    Add a sympy Printer to stringify sympy expressions in a petab-compatible way.
    For example, we need to avoid `str(sympy.sympify("x^2"))` -> `'x**2'`.
    
    Closes #362.

--- petab/v1/math/__init__.py
+++ petab/v1/math/__init__.py
@@ -1,3 +1,4 @@
 """Functions for parsing and evaluating mathematical expressions."""
 
+from .printer import PetabStrPrinter, petab_math_str  # noqa: F401
 from .sympify import sympify_petab  # noqa: F401
