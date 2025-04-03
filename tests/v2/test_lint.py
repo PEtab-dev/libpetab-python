d1f692f733b957d3cceff4844f6182aeef893c7f
Daniel Weindl <daniel.weindl@uni-bonn.de>
2025-04-03 13:38:00 +0200

    v2: Allow applying multiple conditions simultaneously

--- tests/v2/test_lint.py
+++ tests/v2/test_lint.py
@@ -3,8 +3,8 @@
 from copy import deepcopy
 
 from petab.v2 import Problem
-from petab.v2.C import *
 from petab.v2.lint import *
+from petab.v2.models.sbml_model import SbmlModel
 
 
 def test_check_experiments():
@@ -21,3 +21,19 @@ def test_check_experiments():
     tmp_problem = deepcopy(problem)
     tmp_problem["e1"].periods[0].time = tmp_problem["e1"].periods[1].time
     assert check.run(tmp_problem) is not None
+
+
+def test_check_incompatible_targets():
+    """Multiple conditions with overlapping targets cannot be applied
+    at the same time."""
+    problem = Problem()
+    problem.model = SbmlModel.from_antimony("p1 = 1; p2 = 2")
+    problem.add_experiment("e1", 0, "c1", 1, "c2")
+    problem.add_condition("c1", p1="1")
+    problem.add_condition("c2", p1="2", p2="2")
+    check = CheckValidConditionTargets()
+    assert check.run(problem) is None
+
+    problem["e1"].periods[0].condition_ids.append("c2")
+    assert (error := check.run(problem)) is not None
+    assert "overlapping targets {'p1'}" in error.message
