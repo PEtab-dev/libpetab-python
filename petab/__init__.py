40db0bdb5938c19203ea8d4fd6f557ac31f04ba7
Daniel Weindl <daniel.weindl@uni-bonn.de>
2025-11-15 13:39:58 +0100

    backport import fixes

--- petab/__init__.py
+++ petab/__init__.py
@@ -8,6 +8,7 @@ Attributes:
         PEtab should use for operations that can be performed in parallel.
         By default, all operations are performed sequentially.
 """
+
 import importlib
 import sys
 from functools import partial
@@ -23,7 +24,9 @@ def __getattr__(name):
         return attr
     if name == "v1":
         return importlib.import_module("petab.v1")
-    if name != "__path__":
+    if name == "v2":
+        return importlib.import_module("petab.v2")
+    if name not in ("__path__", "__all__", "__wrapped__"):
         warn(
             f"Accessing `petab.{name}` is deprecated and will be removed in "
             f"the next major release. Please use `petab.v1.{name}` instead.",
@@ -33,8 +36,8 @@ def __getattr__(name):
     return getattr(importlib.import_module("petab.v1"), name)
 
 
-def v1getattr(name, module):
-    if name != "__path__":
+def _v1getattr(name, module):
+    if name not in ("__path__", "__all__", "__wrapped__"):
         warn(
             f"Accessing `petab.{name}` is deprecated and will be removed in "
             f"the next major release. Please use `petab.v1.{name}` instead.",
@@ -64,7 +67,7 @@ for v1_object in v1_objects:
         real_module = importlib.import_module(
             f"petab.v1.{'.'.join(v1_object_parts)}"
         )
-        real_module.__getattr__ = partial(v1getattr, module=real_module)
+        real_module.__getattr__ = partial(_v1getattr, module=real_module)
         sys.modules[module_name] = real_module
     except ModuleNotFoundError:
         pass
