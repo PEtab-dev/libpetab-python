5fc6a3138b2db0507333bf0e59c1615ef93afdd1
Maren Philipps <maren.philipps@uni-bonn.de>
2026-05-26 13:09:11 +0200

    Assign SciML-specific lint checks

--- petab/v2/lint.py
+++ petab/v2/lint.py
@@ -992,6 +992,40 @@ class CheckMappingTable(ValidationTask):
         return None
 
 
+class CheckHybridizationTable(ValidationTask):
+    """Validate the SciML hybridization table."""
+
+    def run(self, problem: Problem) -> ValidationIssue | None:
+        messages = []
+
+        condition_targets = {
+            c.target_id for ct in problem.conditions for c in ct.changes
+        }
+        nn_input_ids = {
+            inp.input_id for nn in problem.neural_networks for inp in nn.inputs
+        }
+        hyb_target_ids = {hyb.target_id for hyb in problem.hybridizations}
+        hyb_target_vals = {hyb.target_value for hyb in problem.hybridizations}
+
+        # Hybridization targets are not also targets in the condition table
+        if culprits := (hyb_target_ids & condition_targets):
+            messages.append(
+                f"Hybridization target ids `{culprits}` are also "
+                "target ids in the condition table."
+            )
+        # NN inputs are not used as target values
+        if culprits := (hyb_target_vals & nn_input_ids):
+            messages.append(
+                "The following neural net inputs were used as target values "
+                f"in the Hybridization tbale: `{culprits}`. Please simplify."
+            )
+
+        if messages:
+            return ValidationError("\n".join(messages))
+
+        return None
+
+
 def get_valid_parameters_for_parameter_table(
     problem: Problem,
 ) -> set[str]:
@@ -1192,3 +1226,13 @@ default_validation_tasks = [
     CheckInitialChangeSymbols(),
     CheckMappingTable(),
 ]
+
+#: Validation tasks that should be run PEtab SciML problems
+sciml_validation_tasks = [
+    CheckHybridizationTable(),
+] + list(
+    set(default_validation_tasks)
+    - {
+        CheckAllParametersPresentInParameterTable(),
+    }
+)
