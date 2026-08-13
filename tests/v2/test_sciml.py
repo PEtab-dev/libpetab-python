6727939ee21621e62268b84bcff9ec95ccd7c409
Branwen Snelling <branwen.snelling@crick.ac.uk>
2026-08-13 12:34:47 +0100

    fix ruff

--- tests/v2/test_sciml.py
+++ tests/v2/test_sciml.py
@@ -412,7 +412,8 @@ def _add_observable_consuming_nn_output(problem):
 
 
 def test_nn_output_in_observable_formula_not_required_parameter():
-    """NN outputs should not appear in the parameter table, and can appear in observable formulas."""
+    """NN outputs should not appear in the parameter table, and can appear in
+    observable formulas."""
     from petab.v2.lint import get_required_parameters_for_parameter_table
 
     problem = _add_observable_consuming_nn_output(_get_test_problem())
